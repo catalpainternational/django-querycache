700650b24cf3cc1d8f9a496a2988d749e24e39f5
Joshua Brooks <josh.vdbroek@gmail.com>
2021-12-12 22:44:32 +0900

    split tests, add coverage

--- django_querycache/runtests.py
+++ django_querycache/runtests.py
@@ -3,9 +3,9 @@ import os
 import sys
 
 import django
+from coverage import Coverage
 from django.conf import settings
 from django.test.utils import get_runner
-from coverage import Coverage
 
 if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
