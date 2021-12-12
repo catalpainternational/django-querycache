196f73e0de41a1b5a97941121b7f564a533775cd
Joshua Brooks <josh.vdbroek@gmail.com>
2021-12-12 22:34:48 +0900

    introducing coverage

--- django_querycache/runtests.py
+++ django_querycache/runtests.py
@@ -5,11 +5,18 @@ import sys
 import django
 from django.conf import settings
 from django.test.utils import get_runner
+from coverage import Coverage
 
 if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
+    cov = Coverage()
+    cov.erase()
+    cov.start()
     failures = test_runner.run_tests(["tests"])
+    cov.stop()
+    cov.save()
+    covered = cov.report()
     sys.exit(bool(failures))
